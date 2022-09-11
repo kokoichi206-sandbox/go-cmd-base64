37ec0c1206625faf7c9cdc4249a9c72f72530e59
takahiro.tominaga <tt.300607@gmail.com>
2022-09-12 02:01:08 +0900

    EncodeしてDecodeしたら元に戻ることを確認するテストを追加

--- main_test.go
+++ main_test.go
@@ -33,3 +33,29 @@ func TestEncode(t *testing.T) {
 		require.Equal(t, expected, result)
 	}
 }
+
+// Encode して Decode したら元に戻ることの確認。
+func TestEncodeDecode(t *testing.T) {
+	fileNames := []string{
+		"test_data/test0.txt",
+		"test_data/test1.txt",
+		"test_data/cat.jpg",
+	}
+
+	for _, fileName := range fileNames {
+		// Arrange
+		// Encode の引数に渡すために、ファイルを読み込みバイト列にする。
+		buf, err := ioutil.ReadFile(fileName)
+		require.NoError(t, err)
+
+		// Act
+		encodedStr := main.Encode(buf)
+		t.Log(encodedStr)
+		t.Log([]byte(encodedStr))
+		decodedStr := main.Decode([]byte(encodedStr))
+		t.Log(decodedStr)
+
+		// Assert
+		require.Equal(t, buf, []byte(decodedStr))
+	}
+}
